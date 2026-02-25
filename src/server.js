1ecf290a1dab8eb323a3064ffaffc1e5093fb57d
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-25 16:51:02 -0300

    refactor(server): created separate routes   Separating the routes from the main server file.

--- src/server.js
+++ src/server.js
@@ -1,33 +1,18 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
-import { Databasae } from "./database.js";
-import { randomUUID } from "node:crypto";
-
-const database = new Databasae();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-    return res.writeHead(200).end(JSON.stringify(users));
-  }
-
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return res.writeHead(201).end("User Created");
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end("Not Found");
