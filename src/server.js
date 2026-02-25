c1ca43c25c09ee5e7e87d0f353ddfcf08c851085
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-25 15:22:56 -0300

    feat(database) persisting database   Making the database persistent in the application to correctly store the data of the users that will be created.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Databasae } from "./database.js";
 
-const users = [];
+const database = new Databasae();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -9,6 +10,8 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if(method === "GET" && url === "/users") {
+    const users = database.select("users"); 
+
     return res
       .writeHead(200)
       .end(JSON.stringify(users));
@@ -17,11 +20,12 @@ const server = http.createServer(async (req, res) => {
   if(method === "POST" && url === "/users") {
     const { name, email } = req.body;
     
-    users.push({
-      id: users.length + 1,
+    const user = {
       name,
       email,
-    });
+    };
+    
+    database.insert("users", user);
 
     return res
       .writeHead(201)
