a0d8758efdd1248fad925ea4854b405c1b53348e
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-25 16:29:53 -0300

    feat(server): add random user uuid

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { Databasae } from "./database.js";
+import { randomUUID } from "node:crypto";
 
 const database = new Databasae();
 
@@ -9,28 +10,25 @@ const server = http.createServer(async (req, res) => {
 
   await json(req, res);
 
-  if(method === "GET" && url === "/users") {
-    const users = database.select("users"); 
+  if (method === "GET" && url === "/users") {
+    const users = database.select("users");
 
-    return res
-      .writeHead(200)
-      .end(JSON.stringify(users));
-  };
+    return res.writeHead(200).end(JSON.stringify(users));
+  }
 
-  if(method === "POST" && url === "/users") {
+  if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
-    
+
     const user = {
+      id: randomUUID(),
       name,
       email,
     };
-    
+
     database.insert("users", user);
 
-    return res
-      .writeHead(201)
-      .end("User Created");
-  };
+    return res.writeHead(201).end("User Created");
+  }
 
   return res.writeHead(404).end("Not Found");
 });
