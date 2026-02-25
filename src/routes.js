ffde90de10a86060dd393de70d211b28dd597d88
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-25 20:44:57 -0300

    feat(routes): adding user search   Added a search method to the user table, which works by collecting the query parameters using regular expressions.

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,12 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = database.select("users");
-      const params = req.params;
+      const { search } = req.query;
 
-      params.id;
+      const users = database.select("users", search ? {
+        name: search,
+        email: search,
+      } : null);
 
       return res.end(JSON.stringify(users));
     },
